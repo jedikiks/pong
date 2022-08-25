edf2062eb072c9a92585d1e2201be60b7716c7b1
jedikiks <57885938+jedikiks@users.noreply.github.com>
2022-08-25 14:29:54 -0400

    Delete build directory

--- build/CMakeFiles/pong.dir/src/audio.cpp.o.d
+++
@@ -1,6 +0,0 @@
-CMakeFiles/pong.dir/src/audio.cpp.o: \
- /home/cmvogeli/git/jedikiks/pong/src/audio.cpp \
- /usr/include/stdc-predef.h \
- /home/cmvogeli/git/jedikiks/pong/src/../include/audio.hpp \
- /usr/local/include/raylib.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/12.1.1/include/stdarg.h
